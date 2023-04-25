16100a46221dd42d8de280a1692bd06507067a54
jiwagon <jiwagon11@gmail.com>
2023-04-25 05:34:44 -0400

    creating getIndex test cases

--- src/DoublyLinkedListTest.java
+++ src/DoublyLinkedListTest.java
@@ -120,5 +120,47 @@ class DoublyLinkedListTest {
 
     @Test
     void getIndex() {
+        DoublyLinkedList dll4 = new DoublyLinkedList();
+
+        ArrayList<String> femaleArtists = new ArrayList<>();
+        ArrayList<String> maleArtists = new ArrayList<>();
+        femaleArtists.add("Taylor Swift");
+        femaleArtists.add("Lady Gaga");
+        maleArtists.add("Harry Styles");
+        maleArtists.add("Drake");
+
+        Album album1 = new Album(1, femaleArtists,"Born This Way", 10);
+        Album album2 = new Album(2, femaleArtists,"Midnights", 20);
+        Album album3 = new Album(3, femaleArtists, "Love Story",30);
+        Album album4 = new Album(4, maleArtists, "Harry's House",45);
+        Album album5 = new Album(5, maleArtists, "More Life",50);
+        Album album6 = new Album(6, femaleArtists, "Sour",60);
+
+        dll4.insert(0, album1);
+        dll4.insert(1, album2);
+        dll4.insert(2, album3);
+        dll4.insert(3, album4);
     }
+
+    /**
+     * // Test case 1: Get index of an item that exists in the list
+     * int index = doublyLinkedList.getIndex("bar");
+     * System.out.println("Index of 'bar': " + index); // Output: Index of 'bar': 1
+     *
+     * // Test case 2: Get index of an item that does not exist in the list
+     * index = doublyLinkedList.getIndex("xyz");
+     * System.out.println("Index of 'xyz': " + index); // Output: Index of 'xyz': -1
+     *
+     * // Test case 3: Get index of an item in an empty list
+     * DoublyLinkedList<Integer> emptyList = new DoublyLinkedList<>();
+     * index = emptyList.getIndex(10);
+     * System.out.println("Index of 10 in empty list: " + index); // Output: Index of 10 in empty list: -1
+     *
+     * // Test case 4: Get index of an item in a list with only one item
+     * DoublyLinkedList<Integer> singleItemList = new DoublyLinkedList<>();
+     * singleItemList.insert(0, 20);
+     * index = singleItemList.getIndex(20);
+     * System.out.println("Index of 20 in single-item list: " + index); // Output: Index of 20 in single-item list: 0
+     *
+     */
 }
